00f5612dc6b311ecd602844de15114ddf0adec4d
Ilya Nurullin <nurullinin@mail.ru>
2024-07-04 19:50:07 +0300

    code from class (logging, collections, fs)

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Posts\ConfirmDeleteController;
+use App\Http\Middleware\CheckQ;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
@@ -28,4 +33,23 @@ Route::group(['prefix' => '/posts/{post}/comments'], function () {
 Route::get('/int', function (){
     dump(app('error'));
     return "ok";
+});
+
+Route::post('/upload', function (Request $request) {
+    $extension = $request->file('avatar')->getExtension();
+    dump($request->file('avatar'));
+    dump($request->file('avatar')->getFilename());
+    dd($extension);
+    dump($request->file('avatar')->storeAs('avatar/img.'.$extension));
+    return 'ok';
+});
+
+Route::get('/f', function (){
+    return '
+<form action="/upload" method="post" enctype="multipart/form-data">
+<input type="hidden" name="_token" value="'.csrf_token().'">
+<input type="file" name="avatar">
+<button>Send</button>
+</form>
+    ';
 });
